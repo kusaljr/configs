a6439973335bb3d531568225bbe5e1bb0197b966
unknown <prastutlamsal41@gmail.com>
2022-04-25 11:03:25 -0700

    predictor and z added

--- Powershell/user_profile.ps1
+++ Powershell/user_profile.ps1
@@ -1,9 +1,7 @@
 # Prompt
 Import-Module posh-git
 Import-Module oh-my-posh
-Import-Module -Name Terminal-Icons
-
-Set-PSReadLineOption -PredictionSource History
+Set-PoshPrompt Paradox
 
 
 # Load prompt config
@@ -11,6 +9,19 @@ function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName}
 $PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'kusaljr.omp.json'
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl-d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl-f' -PSReadlineChordReverseHistory 'Ctrl-r'
 
 
 # Alias
